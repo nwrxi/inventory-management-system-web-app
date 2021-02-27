619c74ac90f8f96b5eab38719cdb33b5a487caea
Aleks <aleksf11125@outlook.com>
2021-02-27 10:15:39 +0100

    Add mobx user store

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,5 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import { createContext } from "react";
+import { history } from "..";
 import axiosAgent from "../api/axiosAgent";
 import { IUser, IUserFormValues } from "../models/User";
 import { BaseStore } from "./BaseStore";
@@ -18,10 +19,35 @@ export default class UserStore {
       const user = await axiosAgent.login(userToLogin);
       if (user) {
         runInAction(() => (this.user = user));
+        window.localStorage.setItem("token", user.token);
+        history.push("/inventory");
       }
-      window.localStorage.setItem("token", user.token);
     } catch (error) {
       throw error;
+    }
+  };
+
+  register = async (userToRegister: IUserFormValues) => {
+    try {
+      const user = await axiosAgent.register(userToRegister);
+      if (user) {
+        runInAction(() => (this.user = user));
+        window.localStorage.setItem("token", user.token);
+        history.push("/inventory");
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  getUser = async () => {
+    try {
+      const user = await axiosAgent.currentUser();
+      runInAction(() => {
+        this.user = user;
+      });
+    } catch (error) {
+      console.log(error);
     }
   };
 }
